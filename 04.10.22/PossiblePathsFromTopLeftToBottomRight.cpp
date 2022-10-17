949efc4ad4d965432fa701be3e5a2b9c569ce4d5
malay44 <malay.p1@ahduni.edu.in>
2022-10-17 16:28:37 +0530

    07 October 2022 GFG

---
+++ 04.10.22/PossiblePathsFromTopLeftToBottomRight.cpp
@@ -0,0 +1,67 @@
+//https://practice.geeksforgeeks.org/problems/count-all-possible-paths-from-top-left-to-bottom-right3011/1
+
+class Solution {
+  public:
+  long long int M = 1000000007;
+unsigned long long power(unsigned long long x,
+                                  int y, int p)
+{
+    unsigned long long res = 1; // Initialize result
+ 
+    x = x % p; // Update x if it is more than or
+    // equal to p
+ 
+    while (y > 0)
+    {
+     
+        // If y is odd, multiply x with result
+        if (y & 1)
+            res = (res * x) % p;
+ 
+        // y must be even now
+        y = y >> 1; // y = y/2
+        x = (x * x) % p;
+    }
+    return res;
+}
+ 
+// Returns n^(-1) mod p
+unsigned long long modInverse(unsigned long long n, 
+                                            int p)
+{
+    return power(n, p - 2, p);
+}
+ 
+// Returns nCr % p using Fermat's little
+// theorem.
+unsigned long long nCrModPFermat(unsigned long long n,
+                                 int r, int p)
+{
+    // If n<r, then nCr should return 0
+    if (n < r)
+        return 0;
+    // Base case
+    if (r == 0)
+        return 1;
+ 
+
+    unsigned long long fac[n + 1];
+    fac[0] = 1;
+    for (int i = 1; i <= n; i++)
+        fac[i] = (fac[i - 1] * i) % p;
+ 
+    return (fac[n] * modInverse(fac[r], p) % p
+            * modInverse(fac[n - r], p) % p)
+           % p;
+}
+ 
+
+    long long int numberOfPaths(int m, int n){
+        // code here
+        long long int mini = min(m-1,n-1);
+        long long int temp = m+n-2;
+       long long int ans =  nCrModPFermat(temp,mini,M);
+       return ans;
+
+    }
+};
