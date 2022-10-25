99e212f331983761678e4f384ebc7e983cf231fb
memdani <yansi.m@ahduni.edu.in>
2022-10-25 11:57:14 +0530

    20 october 2022, Leetcode

--- 18.10.22/CountandSay.cpp
+++ 18.10.22/CountandSay.cpp
@@ -1,40 +1,33 @@
-// Question Link: https://leetcode.com/problems/count-and-say/
-
+// write in template; int main code already given by leetcode.
 class Solution {
 public:
     string countAndSay(int n) {
-        vector<pair<char,int>> v;
-        string s="1";
-        if(n==1){
-              return s;
-        }
+        string previous_str="1";
         for(int i=1;i<n;i++){
-            v=solve(s);
-            s=ans(v);
-        }
-        return s;
-    }
-    vector<pair<char,int>> solve(string s){
-        vector<pair<char,int>> v;
-        int cnt=1;
-        for(int i=1;i<s.size();i++){
-            if(s[i]==s[i-1]){
-                cnt++;
+            int cnt=0;
+            char c=previous_str[0];
+            string temp="";
+            for(int j=0;j<previous_str.size();j++){
+                if(previous_str[j]==c){
+                    cnt++;
+                }
+                else{
+                    temp+=('0'+cnt);
+                    temp+=c;
+                    cnt=1;
+                    c=previous_str[j];
+
+                }
             }
-            else{
-                v.push_back({s[i-1],cnt});
-                cnt=1;
+            
+            if(cnt>0){
+                temp+=('0'+cnt);
+                temp+=c;
             }
+            previous_str=temp;
+            // cout<<previous_str<<" ";
         }
-        v.push_back({s[s.size()-1],cnt});
-        return v;
-    }
-    string ans(vector<pair<char,int>> v){
-        string s="";
-        for(int i=0;i<v.size();i++){
-            s+=to_string(v[i].second);
-            s+=v[i].first;
-        }
-        return s;
+        
+        return previous_str;
     }
 };
